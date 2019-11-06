76771e79309daa22f7876a7f00a99c00aa2135b5
Miras <rahimbaevm97@gmail.com>
2019-11-06 22:27:33 +0600

    removing web parts of django

--- channelsproj/routing.py
+++ channelsproj/routing.py
@@ -1,9 +1,9 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
-from notifier.consumers import EchoConsumer
+from notifier.consumers import NoseyConsumer
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
-        path("ws/", EchoConsumer),
+        path("notifications/", NoseyConsumer),
     ])
 })
